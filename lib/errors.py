2874c32812ee34359b40b6f8c9b3f326023ab52c
bomattin <bomattin@butler.edu>
2015-09-16 14:09:37 -0400

    Implement error message types

---
+++ lib/errors.py
@@ -0,0 +1,19 @@
+__author__ = 'bretmattingly'
+
+
+class MissingComponentsError(Exception):
+    def __init__(self, mtype, required, provided):
+        self.req = set(required)
+        self.pro = set(provided)
+        self.mtype = mtype
+
+    def __str__(self):
+        return repr("Message type " + self.mtype + "missing components: " + (self.req - self.pro))
+
+
+class UnknownTypeError(Exception):
+    def __init__(self, mtype):
+        self.mtype = mtype
+
+    def __str__(self):
+        return repr("Unknown message type: " + self.mtype)
