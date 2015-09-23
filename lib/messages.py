a7697d3bbea176d20e142ea4860cd005910eb884
bomattin <bomattin@butler.edu>
2015-09-23 12:59:42 -0400

    Conform to GGMP Spec v0.2.0

--- lib/messages.py
+++ lib/messages.py
@@ -3,65 +3,35 @@ __author__ = 'bretmattingly'
 
 class Action:
     def __init__(self, ACK, cl, mid, ar, an, ac1, ac2):
-        self.stream = bytearray([0x01]) if ACK else bytearray([0x00])
+        self.stream = bytearray([0x00]) if ACK else bytearray([0x01])
         self.stream.extend((cl & 0xFFFFFF).to_bytes(3, "big", signed=False))
-        self.stream.extend((mid & 0xFFFFFF).to_bytes(3, "big", signed=False))
-        self.stream.extend((ar & 0xFFFFFF).to_bytes(3, "big", signed=False))
-        self.stream.extend((an & 0xFFFF).to_bytes(2, "big", signed=False))
+        self.stream.extend((mid & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
+        self.stream.extend((ar & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
+        self.stream.extend((an & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
         self.stream.extend((ac1 & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
         self.stream.extend((ac2 & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
 
 
 class ActionShort:
     def __init__(self, ACK, cl, mid, ar, an, ac1, ac2):
-        self.stream = 0x02
-        if ACK:
-            self.stream += 0x01
-        self.stream <<= 8 * 1
-
-        self.stream += (cl & 0xFF)
-        self.stream <<= 8 * 1
-
-        self.stream += (mid & 0xFFFF)
-        self.stream <<= 8 * 2
-
-        self.stream += (ar & 0xFF)
-        self.stream <<= 8 * 1
-
-        self.stream += (an & 0xFF)
-        self.stream <<= 8 * 1
-
-        self.stream += (ac1 & 0xFF)
-        self.stream <<= 8 * 1
-
-        self.stream += (ac2 & 0xFF)
-        self.stream <<= 8 * 1
+        self.stream = bytearray([0x02]) if ACK else bytearray([0x03])
+        self.stream.extend((cl & 0xFF).to_bytes(1, "big", signed=False))
+        self.stream.extend((mid & 0xFFFF).to_bytes(2, "big", signed=False))
+        self.stream.extend((ar & 0xFF).to_bytes(1, "big", signed=False))
+        self.stream.extend((an & 0xFF).to_bytes(1, "big", signed=False))
+        self.stream.extend((ac1 & 0xFF).to_bytes(1, "big", signed=False))
+        self.stream.extend((ac2 & 0xFF).to_bytes(1, "big", signed=False))
 
 
 class ActionExtended:
     def __init__(self, ACK, cl, mid, ar, an, ac1, ac2):
-        self.stream = 0x04
-        if ACK:
-            self.stream += 0x01
-        self.stream <<= 8 * 1
-
-        self.stream += (cl & 0xFFFFFF)
-        self.stream <<= 8 * 3
-
-        self.stream += (mid & 0xFFFFFF)
-        self.stream <<= 8 * 3
-
-        self.stream += (ar & 0xFFFFFF)
-        self.stream <<= 8 * 3
-
-        self.stream += (an & 0xFFFF)
-        self.stream <<= 8 * 2
-
-        self.stream += (ac1 & 0xFFFFFFFF)
-        self.stream <<= 8 * 4
-
-        self.stream += (ac2 & 0xFFFFFFFF)
-        self.stream <<= 8 * 4
+        self.stream = bytearray([0x04]) if ACK else bytearray([0x05])
+        self.stream.extend((cl & 0xFFFFFF).to_bytes(3, "big", signed=False))
+        self.stream.extend((mid & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
+        self.stream.extend((ar & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
+        self.stream.extend((an & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
+        self.stream.extend((ac1 & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
+        self.stream.extend((ac2 & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
 
 
 class Data:
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Python client module for GGMP, the Generic Gameserver Messaging Protocol
+"""
+from setuptools import find_packages, setup
+
+dependencies = []
+
+setup(
+    name='ggmp-client',
+    version='0.2.0',
+    url='https://github.com/bomattin/ggmp-client',
+    license='BSD',
+    author='Bret Mattingly 2',
+    author_email='bret.mattingly@gmail.com',
+    description='Python client module for GGMP, the Generic Gameserver Messaging Protocol',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={},
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        # 'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        # 'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
