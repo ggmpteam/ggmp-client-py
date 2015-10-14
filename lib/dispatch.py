fea49c55cadae34610ae384619604e000d30e4e1
bomattin <bomattin@butler.edu>
2015-10-14 11:41:35 -0400

    First draft of implementations for Dispatch, _Awaitbox

--- lib/dispatch.py
+++ lib/dispatch.py
@@ -2,8 +2,10 @@ __author__ = 'bretmattingly'
 
 import socket
 from threading import local, Thread
-from .decoding import decode as message_decode
 from queue import Queue, Empty
+from .decoding import decode as message_decode
+from .messages import *
+from .errors import *
 
 
 class Dispatch:
@@ -22,42 +24,80 @@ class Dispatch:
     UDP bytes -> inthread -> Dispatch <
                                        inbox -> read()
     """
-    """
+    def __init__(self, c_id, ip_addr):
+        self.threader = _Threader(c_id, ip_addr)
+        self.inbox = Queue()
+        self.outbox = Queue()
+        self.awaitbox = _Awaitbox()
+        self.lostbox = Queue()
 
-    Dispatch needs:
-        - Threader
-        - Inbox
-        - Outbox
-        - Awaitbox
-        - Lostbox
+    def add_message(self, msg):
+        self.outbox.put(msg)
 
-    Threader needs:
-        - Input/Output socket
-        - Server IP
-        - "inbox"
-        - thread definitions
+    def send_all(self):
+        lost = self.awaitbox.tick()
+        for m in lost:
+            self.lostbox.put(m)
 
-    """
-    pass
+        #  Still need to read from awaitbox
+
+        while not self.outbox.empty():
+            try:
+                m = self.outbox.get()
+                if m.ack:
+                    self.awaitbox.add(m)
+                #  "Burst fire"
+                self.threader.add_message(m)
+                self.threader.add_message(m)
+                self.threader.add_message(m)
+            except Empty:
+                break
+
+    def in_process(self):
+        """
+        * Read all from threader.inq
+        * Decode
+        * If any Acks, process awaitbox
+        * Move all non-Acks to inbox
+        """
+        ms = self.threader.read_all()
+        for m in ms:
+            if type(m) == Ack:
+                self.awaitbox.acknowledge(m.mid)
+                continue
+            else:
+                self.inbox.put(m)
 
 
 class _Threader:
     """
     _Threader is the class responsible for managing the input and output threads
     """
-
     def __init__(self, c_id, ip_addr):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Input/Output socket
         self.sock.bind(("0.0.0.0", 12358))
-        self.inq = Queue()  # Input queue
-        self.outq = Queue()  # Output queue
+        self.inq = Queue()  # Input queue, stores Message objects
+        self.outq = Queue()  # Output queue, stores Message objects
         self.server_addr = ip_addr
 
-        self.outthread = Thread(target=sendmsg, daemon=True, args=(self.outq, self.sock, self.server_addr))
         self.inthread = Thread(target=listen, daemon=True, args=(self.inq, self.sock))
+        self.outthread = Thread(target=sendmsg, daemon=True, args=(self.outq, self.sock, self.server_addr))
 
-        self.outthread.start()
         self.inthread.start()
+        self.outthread.start()
+
+    def add_message(self, msg):
+        self.inq.put(msg)
+
+    def read_all(self):
+        ret = []
+        while not self.inq.empty():
+            try:
+                ret.append(self.inq.get())
+            except Empty:
+                pass
+
+        return ret
 
 
 def listen(inq, sin):
@@ -65,7 +105,7 @@ def listen(inq, sin):
         localdata = local()
         localdata.m = sin.recvfrom(512)
         print("Received " + str(localdata.m[0]) + " from " + str(localdata.m[1]))
-        inq.put(localdata.m[0], block=False)
+        inq.put(message_decode(localdata.m[0]), block=False)
         print(message_decode(localdata.m[0]))
 
 
@@ -74,3 +114,39 @@ def sendmsg(outq, sock, ip_addr):
         localdata = local()
         localdata.m = outq.get(block=True)
         sock.sendto(localdata.m.stream, (ip_addr[0], ip_addr[1]))
+
+
+class _Awaitbox:
+    def __init__(self):
+        self.first = dict()
+        self.second = dict()
+        self.third = dict()
+
+    def tick(self):
+        """
+        "Ticks" the awaitbox, moving all items in first to second, second to third, etc.
+        :return: All items which "ticked out" of third (since they should be moved to the lostbox
+        """
+
+        ret = self.third.values()
+        self.third.clear()
+        self.third = self.second.copy()
+        self.second.clear()
+        self.second = self.first.copy()
+        self.first.clear()
+        return ret
+
+    def add(self, msg):
+        self.first[msg.mid] = msg
+
+    def acknowledge(self, id):
+        if id in self.first:
+            del self.first[id]
+        elif id in self.second:
+            del self.second[id]
+        elif id in self.third:
+            del self.third[id]
+        else:
+            print("Acknowledged message " + str(id) + "but don't have it in awaitbox")
+        pass
+
--- lib/messages.py
+++ lib/messages.py
@@ -3,6 +3,14 @@ __author__ = 'bretmattingly'
 
 class Action:
     def __init__(self, ACK, cl, mid, ar, an, ac1, ac2):
+        self.ack = ACK
+        self.cl = cl
+        self.mid = mid
+        self.ar = ar
+        self.an = an
+        self.ac1 = ac1
+        self.ac2 = ac2
+
         self.stream = bytearray([0x00]) if ACK else bytearray([0x01])
         self.stream.extend((cl & 0xFFFFFF).to_bytes(3, "big", signed=False))
         self.stream.extend((mid & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
@@ -14,6 +22,14 @@ class Action:
 
 class ActionShort:
     def __init__(self, ACK, cl, mid, ar, an, ac1, ac2):
+        self.ack = ACK
+        self.cl = cl
+        self.mid = mid
+        self.ar = ar
+        self.an = an
+        self.ac1 = ac1
+        self.ac2 = ac2
+
         self.stream = bytearray([0x02]) if ACK else bytearray([0x03])
         self.stream.extend((cl & 0xFF).to_bytes(1, "big", signed=False))
         self.stream.extend((mid & 0xFFFF).to_bytes(2, "big", signed=False))
@@ -25,6 +41,14 @@ class ActionShort:
 
 class ActionExtended:
     def __init__(self, ACK, cl, mid, ar, an, ac1, ac2):
+        self.ack = ACK
+        self.cl = cl
+        self.mid = mid
+        self.ar = ar
+        self.an = an
+        self.ac1 = ac1
+        self.ac2 = ac2
+
         self.stream = bytearray([0x04]) if ACK else bytearray([0x05])
         self.stream.extend((cl & 0xFFFFFF).to_bytes(3, "big", signed=False))
         self.stream.extend((mid & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
@@ -36,6 +60,12 @@ class ActionExtended:
 
 class Data:
     def __init__(self, ACK, cl, mid, pmsg, siz, dat):
+        self.ack = ACK
+        self.cl = cl
+        self.mid = mid
+        self.pmsg = pmsg
+        self.siz = siz
+        self.dat = dat
         self.stream = bytearray([0x0E]) if ACK else bytearray([0x0F])
         self.stream.extend((cl & 0xFFFFFF).to_bytes(3, "big", signed=False))
         self.stream.extend((mid & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
@@ -46,6 +76,12 @@ class Data:
 
 class DataEnd:
     def __init__(self, ACK, cl, mid, pmsg, siz, dat):
+        self.ack = ACK
+        self.cl = cl
+        self.mid = mid
+        self.pmsg = pmsg
+        self.siz = siz
+        self.dat = dat
         self.stream = bytearray([0x0E]) if ACK else bytearray([0x0F])
         self.stream.extend((cl & 0xFFFFFF).to_bytes(3, "big", signed=False))
         self.stream.extend((mid & 0xFFFFFFFF).to_bytes(4, "big", signed=False))
